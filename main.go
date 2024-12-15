ce4637616b3ed37975541b0ad89754d8ed0a45e2
Esteban Forero <estebanmff@outlook.com>
2024-12-15 12:22:40 -0500

    Fixed security errors

--- main.go
+++ main.go
@@ -86,8 +86,9 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: 100,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
